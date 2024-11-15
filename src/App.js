c52ef9e283df1a7fd72afbb1e9b8ff0ca106c40a
Damian Marquez <marquez.damian@outlook.com>
2024-11-15 16:42:29 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header';
+import NavbarSection from './components/NavbarSection';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './features/home/HomePage';
+import ProfilePage from './features/profile/ProfilePage';
+import SettingsPage from './features/settings/SettingsPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <NavbarSection />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/features/settings/SettingsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SettingsPage.css';
+
+function SettingsPage() {
+  return (
+    <div className="settings-container">
+      <h1>Configuración</h1>
+      <p>Aquí puedes ajustar la configuración de la aplicación.</p>
+    </div>
+  );
+}
+
+export default SettingsPage;
