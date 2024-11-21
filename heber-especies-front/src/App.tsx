25866659a2056f4ab2c54b0c2f6c8fe267b91892
hp0341 <hebertescione@hotmail.com>
2024-11-21 11:57:01 -0300

    add especies

--- heber-especies-front/src/App.tsx
+++ heber-especies-front/src/App.tsx
@@ -2,13 +2,14 @@ import { MisEmisores } from "./components/MisEmisores"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NuevoEmisor } from "./components/NuevoEmisor";
 import { Toaster } from "./components/ui/sonner";
+import { MisEspecies } from "./components/MisEspecies";
 
 export const App = () => {
   return (
     <>
       <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
         <Routes>
-          <Route path="/" element={<MisEmisores />} />
+          <Route path="/" element={<MisEspecies />} />
 
           <Route path="/nuevoEmisor" element={<NuevoEmisor />} />
         </Routes>
