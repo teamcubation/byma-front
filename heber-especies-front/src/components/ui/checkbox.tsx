98f14ea038bd58a465effc7bad2df119c8a0e84f
rogvera <rogger.vera23@gmail.com>
2024-11-25 10:37:01 -0300

    se fixea check

--- heber-especies-front/src/components/ui/checkbox.tsx
+++ heber-especies-front/src/components/ui/checkbox.tsx
@@ -13,7 +13,6 @@ const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
-<<<<<<< HEAD
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
@@ -32,23 +31,3 @@ const Checkbox = React.forwardRef<
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
  
 export { Checkbox }
-=======
-<CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
-      className
-    )}
-    {...props}
->
-<CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
->
-<Check className="h-4 w-4" />
-</CheckboxPrimitive.Indicator>
-</CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
- 
-export { Checkbox }
->>>>>>> 681a42910bb192182f245154bc6bb2c90ae05553
