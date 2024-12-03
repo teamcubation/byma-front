f6c41817d9776a24edf44467c0697a6f59f77699
maxizelazny <maximiliano.zelazny@tecval.com.ar>
2024-12-03 10:55:01 -0300

    input arreglado

--- heber-especies-front/vite.config.ts
+++ heber-especies-front/vite.config.ts
@@ -1,18 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `$injectedColor: orange;`
-      }
-    }
-  }
+  },
 })
+
