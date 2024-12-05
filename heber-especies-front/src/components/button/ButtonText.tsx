97e976bd2023cbb77c8fe563ea22cd43a0a4b11e
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-12-05 17:55:31 -0300

    Refactorizo ButtonText

--- heber-especies-front/src/components/button/ButtonText.tsx
+++ heber-especies-front/src/components/button/ButtonText.tsx
@@ -7,16 +7,35 @@ interface ButtonTextProps {
   disable?: boolean;     // Indica si el botón está deshabilitado
   className?: string;   // Clases adicionales para personalización
   icon?: string;        // Icono del botón
+  size?: 'small' | 'medium' | 'large'; // Tamaño del botón
 }
 
-const ButtonText = ({ onClick, text, disable = false, className = "", icon = "" }: ButtonTextProps) => {
+const ButtonText = ({  onClick, 
+    text, 
+    disable = false, 
+    className = "", 
+    icon = "",
+    size = 'medium'}: ButtonTextProps) => {
+        const baseClass = 'button';
+        const sizeClass = `button__size--${size}`;
+        const iconClass = icon ? '' : 'button--no-icon';
+      
+        const buttonClasses = [
+          baseClass,
+          sizeClass,
+          className.includes('button--terciary') ? 'button--terciary' : baseClass,
+          className,
+          iconClass,
+        ].join(' ');
+
     return (
-        <button onClick={onClick}
+        <button 
+            onClick={onClick}
             disabled={disable}
-            className={`${className.includes('button--terciary') ? 'button--terciary' : 'button'} button__size--medium ${className} ${!icon ? 'button--no-icon' : ''}`}>
+            className={buttonClasses}>
             {icon && (
                 <div className="button__icon-wrapper">
-                    <div className={`button__icon ${icon}`}></div>
+                    <img src={icon} alt="icon" />
                 </div>
             )}
 
