e75a5def00c18ab697c91ab80f2bd806224b4e53
Tavo-94 <gustavo.mph@gmail.com>
2024-12-06 11:55:25 -0300

    extraida la logica del login

--- heber-especies-front/src/components/features/login/LoginPage.tsx
+++ heber-especies-front/src/components/features/login/LoginPage.tsx
@@ -14,6 +14,7 @@ import { z } from 'zod'
 import logoByma from '@/assets/img/BYMA-Logo-Login.svg'
 import logoBymaBlanco from '@/assets/img/BYMA-Logo-Login-2.svg'
 import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import useAuth from '@/utils/hooks/useAuth'
 
 type Props = {}
 
@@ -41,15 +42,10 @@ const formSchema = z.object({
 
 function LoginPage({ }: Props) {
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
 
-
-    const togglePasswordVisibility = () => {
-        setShowPassword((visible) => !visible);
-    };
+    const { login, isLoading, isError, errorMessage, loginResponse, isAuthenticated } = useAuth();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -60,64 +56,70 @@ function LoginPage({ }: Props) {
         }
     })
 
-
     async function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
-        setIsLoading(true);
-
-
-        try {
-            const response = await axios.post<LoginResponse>('https://dummyjson.com/auth/login', values);
-            console.log(response.data);
-            window.localStorage.setItem('accessToken', response.data.accessToken);
-            setIsLoading(false);
-            navigate('/abm-gerentes');
-
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                form.setError("root", { message: error.response?.data?.message || "Error en el inicio de sesión" });
-            } else {
-                form.setError("root", { message: "Error en el inicio de sesión" });
-            }
-        } finally {
-            setIsLoading(false);
-        }
-
+        await login(values);
     }
 
     useEffect(() => {
-        const accessToken = window.localStorage.getItem('accessToken');
-        if (accessToken) {
-            navigate('/abm-gerentes');
+        if (isError) {
+            form.setError("root", { message: errorMessage });
+            toast.error(errorMessage);
+            return;
+        }
+        if(loginResponse) {
+            toast.success("Bienvenido " + loginResponse.firstName + " " + loginResponse.lastName);
         }
 
-        //TODO: definir bien como se debe comportar el front en caso de querer acceder a una pagina sin estar logueado
+        //debugger
         if (searchParams.get('message')) {
-            alert(searchParams.get('message'));
+            toast.error(searchParams.get('message'));
         }
-    }, []);
+
+        if (isAuthenticated) {
+            navigate("/abm-gerentes");
+        }
+    }, [isAuthenticated, isError, searchParams, loginResponse]);
 
     return (
         <>
-            <section className={`w-full min-h-screen bg-[#152D5C]`}>
-                <div className='h-full px-6 bg-contain bg-center bg-no-repeat bg-fondo-login grid grid-cols-12 gap-6'>
-                    <Card className='bg-[#1090921A] rounded-[50px] px-10 col-start-8 col-span-4 border-none shadow-black shadow-lg backdrop-blur-md place-self-center'>
-                        <CardHeader className='relative border-b-2 border-verde-dark min-h-[250px] flex justify-center items-center'>
+            <section className={`w-full min-h-[100dvh] grid grid-rows-1 grid-cols-1 bg-[#152D5C]`}>
+                <div className='relative px-6 bg-contain bg-center bg-no-repeat bg-fondo-login grid grid-cols-12 grid-rows-1 content-stretch gap-6'>
+                    {
+                        //TODO: extraer este codigo a un componente "Alerta"
+                        form.formState.errors.root && (
+                            <div className='absolute min-w-96 top-0 left-1/2 transform translate-y-1/4 -translate-x-1/2 text-white border-2 border-l-8 border-red-500 rounded-e-lg ps-2 pe-4 py-2 flex flex-row justify-between gap-3 z-10'>
+                                <div className='flex gap-8'>
+                                    <div className='h-9 w-9 flex items-center justify-center'>
+                                        <InfoIcon className="text-red-500" strokeWidth={2} size={24} />
+                                    </div>
+                                    <div>
+                                        <h3 className='text-base font-bold'>Error al iniciar sesión</h3>
+                                        <p className='text-sm'>{form.formState.errors.root.message}</p>
+                                    </div>
+                                </div>
+                                <Button className='' variant="ghost" size="icon" onClick={() => form.reset()}>
+                                    <X strokeWidth={2} size={24} />
+                                </Button>
+                            </div>
+                        )
+                    }
+                    <Card className='bg-[#1090921A] rounded-[50px] px-10 col-start-8 col-span-4 border-none shadow-black shadow-lg backdrop-blur-md flex flex-col'>
+                        <CardHeader className='relative py-8 h-fit border-b-2 border-verde-dark flex justify-center items-center'>
                             <p className='w-3/4 text-white text-2xl text-center'>La plataforma de BYMA para registrar tus operaciones Bilaterales.</p>
                             <div className='absolute bottom-0 left-3 transform translate-y-1/2 -translate-x-2/3'>
                                 <img src={logoByma} alt="logo byma fondo azul" className='w-[360px]' />
                             </div>
                         </CardHeader>
-                        <CardContent>
-                            <div className='flex flex-col gap-16'>
+                        <CardContent className='pt-24'>
+                            <div className='h-full flex flex-col gap-4'>
                                 <div>
-                                    <h2 className='text-3xl mb-5 font-semibold text-white mt-24'>Bienvenido</h2>
+                                    <h2 className='text-3xl mb-5 font-semibold text-white'>Bienvenido</h2>
                                     <p className='text-white text-2xl'>Accedé con tu usuario y contraseña</p>
                                 </div>
 
 
                                 <Form {...form}>
-                                    <form className={`flex flex-col gap-4 rounded-lg`} onSubmit={form.handleSubmit(onSubmit)}>
+                                    <form className={`flex flex-col gap-4`} onSubmit={form.handleSubmit(onSubmit)}>
                                         <FormField
                                             control={form.control}
                                             name="username"
@@ -158,7 +160,7 @@ function LoginPage({ }: Props) {
 
                                                             <Input
                                                                 className={`pl-12 pr-10 text-white ${form.formState.errors.password && "border-red-500"}`}
-                                                                type={showPassword ? "text" : "password"}
+                                                                type="password"
                                                                 placeholder='••••••••••••••'
                                                                 disabled={isLoading}
                                                                 {...field}
@@ -167,21 +169,6 @@ function LoginPage({ }: Props) {
                                                             {form.formState.errors.password && (
                                                                 <CircleX className="absolute right-4 top-1/2 transform -translate-y-1/2 text-red-500" strokeWidth={1} size={24} />
                                                             )}
-                                                            {/* <Button
-                                                            type="button"
-                                                            variant="ghost"
-                                                            size="icon"
-                                                            className="absolute right-1 top-1/2 transform -translate-y-1/2"
-                                                            onClick={togglePasswordVisibility}
-                                                        >
-                                                            {showPassword ? (
-                                                                <EyeOff className="text-gray-400" strokeWidth={1} size={24}/>
-                                                            ) : (
-                                                                <Eye className=" text-gray-400" strokeWidth={1} size={24}/>
-                                                            )}
-                                                            <span className="sr-only">canbiar visibilidad de la contraseña</span>
-                                                        </Button> */}
-
                                                         </div>
                                                     </FormControl>
                                                     <FormMessage className='absolute -bottom-1 left-0 transform translate-y-full' />
@@ -198,34 +185,17 @@ function LoginPage({ }: Props) {
                                                 <Button type='submit' className='w-full bg-white' disabled={isLoading}>Enviar</Button>
                                             )
                                         }
+                                        <Link to="/" className='text-white text-sm'>¿Olvidaste la contraseña?</Link>
 
                                     </form>
 
                                 </Form>
+
                             </div>
                         </CardContent>
-                        <CardFooter className='relative flex-col gap-28 mb-16 items-center justify-center'>
-                            <Link to="/" className='text-white text-sm'>¿Olvidaste la contraseña?</Link>
-                            {
-                                //TODO: extraer este codigo a un componente "Alerta"
-                                form.formState.errors.root && (
-                                    <div className='absolute top-0 transform translate-y-1/2 w-full text-white border-2 border-l-8 border-red-500 rounded-e-lg ps-2 pe-4 py-2 flex flex-row justify-between gap-3'>
-                                        <div className='flex gap-8'>
-                                            <div className='h-9 w-9 flex items-center justify-center'>
-                                                <InfoIcon className="text-red-500" strokeWidth={2} size={24} />
-                                            </div>
-                                            <div>
-                                                <h3 className='text-base font-bold'>Error al iniciar sesión</h3>
-                                                <p className='text-sm'>{form.formState.errors.root.message}</p>
-                                            </div>
-                                        </div>
-                                        <Button className='' variant="ghost" size="icon" onClick={() => form.reset()}>
-                                            <X strokeWidth={2} size={24} />
-                                        </Button>
-                                    </div>
-                                )
-                            }
-                            <img src={logoBymaBlanco} alt="logo Byma" />
+                        <CardFooter className='flex-grow relative flex justify-center items-end'>
+
+                            <img src={logoBymaBlanco} alt="logo Byma" className='w-48' />
                         </CardFooter>
                     </Card>
 
